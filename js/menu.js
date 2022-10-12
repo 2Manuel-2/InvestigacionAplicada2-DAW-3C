d8b6c2759a8c23865688846e44104208cf067a30
2Manuel-2 <99776318+2Manuel-2@users.noreply.github.com>
2022-10-11 21:50:10 -0600

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -3,15 +3,11 @@
     const menu = document.querySelector('.nav__link');
     const closeMenu = document.querySelector('.nav__close');
 
+    
     openButton.addEventListener('click', ()=>{
         menu.classList.add('nav__link--show');
     });
-
     closeMenu.addEventListener('click', ()=>{
         menu.classList.remove('nav__link--show');
     });
-
-    
-
-
 })();
